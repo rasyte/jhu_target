01cb6b66e8421d55ca3b1b26204ea155afd4d566
rasyte <rasar@hotmail.com>
2020-04-29 08:08:20 -0700

    fix build

--- common/common.h
+++ common/common.h
@@ -1,8 +1,9 @@
 #ifndef _common_h_
 #define _common_h_
 
-#include <QPoint>
-#include <QMap>
+//#include <QPoint>
+//#include <QMap>
+
 
 enum CMDS { CMD_HRT_BEAT = 0, CMD_UPDATE_STATE, CMD_SUGGEST, CMD_ACCUSE, CMD_INIT, CMD_PLAYER_JOIN, CMD_SHUTDOWN, 
     CMD_GAME_BEGIN, CMD_GAME_SELECT, CMD_MOVE, CMD_GAME_OVER, CMD_TURN, CMD_TURN_OVER, CMD_ACCUSE_RSP, CMD_SUGGEST_RSP, CMD_UNUSED };
@@ -18,7 +19,7 @@ static const int cntSuspects = sizeof(lpszSuspects) / sizeof(lpszSuspects[0]);
 static const int cntWeapons = sizeof(lpszWeapons) / sizeof(lpszSuspects[0]);
 static const int cntRooms = sizeof(lpszRooms) / sizeof(lpszRooms[0]);
 
-static const QPoint  startLoc[6] = { {460, 30}, {640, 190}, {460, 585}, {210, 585}, {30, 430},{30, 190} };
+//static const QPoint  startLoc[6] = { {460, 30}, {640, 190}, {460, 585}, {210, 585}, {30, 430},{30, 190} };
 
 #ifdef __WIN
 #pragma pack(show)
