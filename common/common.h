1e6a5a2b7163c2a9dfb1fd50a86d3d60f9476733
rasyte <rasar@hotmail.com>
2020-04-29 12:38:57 -0700

    remove QPoint from common

--- common/common.h
+++ common/common.h
@@ -1,8 +1,8 @@
 #ifndef _common_h_
 #define _common_h_
 
-#include <QPoint>
-#include <QMap>
+//#include <QPoint>
+//#include <QMap>
 
 
 enum CMDS { CMD_HRT_BEAT = 0, CMD_UPDATE_STATE, CMD_SUGGEST, CMD_ACCUSE, CMD_INIT, CMD_PLAYER_JOIN, CMD_SHUTDOWN, 
